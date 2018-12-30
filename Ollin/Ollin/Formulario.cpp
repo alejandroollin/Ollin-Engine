85245cd5ea5b09a7f7c394ba4892f9a1fe080c50
ollinengine@gmail.com <ollinengine@gmail.com>
2018-12-29 19:28:14 -0600

    f

--- Ollin/Ollin/Formulario.cpp
+++ Ollin/Ollin/Formulario.cpp
@@ -1,2 +1,13 @@
 #include "Formulario.h"
 
+using namespace Ollin    // Reemplazar Project1 por el nombre que se le haya dado al proyecto
+
+[STAThread]
+
+void main(array<String^>^ arg) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	//Project1 es el nombre del proyecto
+	Formulario form;	//Reemplazar MyForm por el nombre que tenga tu clase 
+	Application::Run(%form);
+}
