1cf5e5ccd9aec35f086f7cd72d704561702023dd
ollinengine@gmail.com <ollinengine@gmail.com>
2018-12-29 19:50:14 -0600

    Redimencionar

--- Ollin/Ollin/Formulario.h
+++ Ollin/Ollin/Formulario.h
@@ -48,11 +48,17 @@ namespace Ollin {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"Formulario";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->SuspendLayout();
+			// 
+			// Formulario
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(477, 96);
+			this->Name = L"Formulario";
+			this->Text = L"Formulario";
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
 	};
